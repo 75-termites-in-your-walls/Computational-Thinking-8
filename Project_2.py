224b1861a664297d325205acaf9c5b8bc1338879
75-termites-in-your-walls <magnusisdal@seattleacademy.org>
2026-03-17 16:04:28 +0000

    sup

---
+++ Project_2.py
@@ -0,0 +1,7 @@
+print("Welcome to the grammatical games!!! where we ask questions about any sort of thing surronding lingustics")
+
+print("EXAMPLE1")
+name=input
+print(F"Hello {name}, welcome to the game!!!")
+input()
+print("Question 1: which langauge is ")
--- project 1.py
+++ project 1.py
@@ -1,13 +1,19 @@
+print ("EXAMPLE1")
+name=input("What is your name?")
+print(F"hello {name} and welcome to 2 truths and a lie")
+input()
+print ("first few questions!")
+print ("I have been to the Washington fair,")
+print ("I had a cat,")
+print ("I have coded before")
 
-name=input()
-
-elif
 
 
-
-
-
-
+answear = input
+if answear == 1
+print ("correct, I have never been to a state fair")
+else:
+print ("incorrect,the correct answear was one")
 
 
 
